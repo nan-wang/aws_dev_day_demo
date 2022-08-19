76a7214ddcad95c3f56845b4fddd6c5d04181c54
Nan Wang <nan.wang@jina.ai>
2022-08-20 01:13:34 +0800

    feat: fix the status

--- app.py
+++ app.py
@@ -1,103 +1,126 @@
 import streamlit as st
 from docarray import Document
+from enum import Enum
+
+class Status(Enum):
+    PROMPT = 1
+    DALLE = 2
+    DIFFUSION = 3
+    UPSCALE = 4
 
 
-def get_prompt(text):
-    scenario = text
-    if not text:
-        return None
-    return f'a whimsical child book illustration of the red bus and {scenario}, the style charming, childlike, carefree, dreamy, fun and colorful'
-    # return f'a children book illustration of a red bus and {scenario}, the style of Linh Pham'
-    # return f'a children book illustration of a red bus and {scenario}, the style of Studio Ghibli'
+def get_prompt():
+    if st.session_state.status.value != Status.PROMPT.value:
+        return
+    plot_tile()
+    st.header('第一步：一句话接龙故事')
+    st.text('We are driving a red bus to <story from the last teller>')
+    st.subheader('We are driving a red bus to ...')
+    st.text_input('',
+                  key='prompt_raw',
+                  placeholder='a mountain / a flower field / a bridge over the river / take a picnic',
+                  on_change=get_from_dalle)
 
 
-@st.cache(allow_output_mutation=True)
-def get_from_dalle(prompt):
-    doc = Document(text=prompt).post(server_url, parameters={'num_images': 3})
-    return doc, doc.matches
-
-
-@st.cache(allow_output_mutation=True)
-def get_from_diffusion(doc):
-    return doc.post(f'{server_url}', parameters={'skip_rate': 0.6, 'num_images': 6},
-             target_executor='diffusion').matches
-
-
-@st.cache(allow_output_mutation=True)
-def get_from_upscale(doc):
-    return doc.post(f'{server_url}/upscale')
-
-
-server_url = 'grpcs://dalle-flow.dev.jina.ai'
-drafts = None
-doc = None
-fav = None
-fav_id = None
-dfav = None
-dfav_id = None
-final_fav = None
-
-st.session_state['fav_id_str'] = ''
-st.session_state['dfav_id_str'] = ''
-
-st.title('让我们一起给孩子讲故事')
-st.text('一个关于开巴士去旅行的故事🚌')
-
-st.header('第一步：一句话描述你的故事')
-st.text_input('We are driving a red bus to ...',
-              key='prompt_raw',
-              placeholder='a mountain / a flower field / a bridge over the river / take a picnic')
-
-# extract the key entity from st.session_state.prompt_raw
-prompt = get_prompt(st.session_state.prompt_raw)
-
-
-if prompt:
-    with st.spinner('正在努力构思✍️...'):
-        doc, drafts = get_from_dalle(prompt)
-
-
-if drafts is not None:
+def get_from_dalle():
+    desc_str = st.session_state.prompt_raw
+    if desc_str:
+        # f'a children book illustration of a red bus and {scenario}, the style of Linh Pham'
+        # f'a children book illustration of a red bus and {scenario}, the style of Studio Ghibli'
+        prompt = f'a whimsical child book illustration of the red bus and {desc_str}, the style charming, childlike, carefree, dreamy, fun and colorful'
+        with st.spinner('正在努力构思✍️...'):
+            try:
+                doc = Document(text=prompt).post(server_url, parameters={'num_images': 3})
+            except Exception as e:
+                st.error(f'failed to call {server_url}, {e}')
+                reset_status()
+                return
+        st.session_state.status = Status.DALLE
+        doc.tags['description'] = f'We are driving a red bus to {st.session_state.prompt_raw}'
+        st.session_state['doc'] = doc
+    if st.session_state.status.value != Status.DALLE.value:
+        return
+    plot_tile()
     st.header('第二步：选择你满意的初稿❤️')
+    st.subheader(f'{st.session_state.doc.tags["description"]}')
     col_list = st.columns(3)
     counter = 0
-    for idx, d in enumerate(drafts):
+    for idx, d in enumerate(doc.matches):
         with col_list[idx%3]:
             st.image(d.uri, caption=f'画稿 {idx+1}')
         counter += 1
-    fav_id_str = st.selectbox(
+    st.selectbox(
         "你满意的初稿❤️",
         [''] + list([f'画稿 {i+1}' for i in range(counter)]),
-        index=0)
-    if fav_id_str:
-        fav_id = int(fav_id_str[3:]) - 1
-        fav = drafts[fav_id]
-        fav.embedding = doc.embedding
+        key='fav_1st_id_str',
+        index=0,
+        on_change=get_from_diffusion)
 
 
-if fav is not None:
-    with st.spinner('正在努力绘制🎨...'):
-        diffused = get_from_diffusion(fav)
+def get_from_diffusion():
+    if st.session_state.get('fav_1st_id_str', ''):
+        fav_id = int(st.session_state.fav_1st_id_str[3:]) - 1
+        fav = st.session_state.doc.matches[fav_id]
+        st.session_state.doc.tags['fav_id'] = fav_id
+        fav.embedding = st.session_state.doc.embedding
+        with st.spinner('正在努力绘制精修图片🎨...'):
+            doc = fav.post(
+                 f'{server_url}',
+                 parameters={'skip_rate': 0.6, 'num_images': 6},
+                 target_executor='diffusion')
+            st.session_state.doc.matches[fav_id].matches = doc.matches
+        st.session_state.status = Status.DIFFUSION
+    if st.session_state.status.value != Status.DIFFUSION.value:
+        return
+    plot_tile()
     st.header('第三步：选择你满意的精修图片❤️')
+    st.subheader(f'{st.session_state.doc.tags["description"]}')
     col_list = st.columns(3)
     counter = 0
-    for idx, d in enumerate(diffused):
-        with col_list[idx%3]:
-            st.image(d.uri, caption=f'画稿 {idx+1}')
+    fav_id = st.session_state.doc.tags['fav_id']
+    for idx, d in enumerate(st.session_state.doc.matches[fav_id].matches):
+        with col_list[idx % 3]:
+            st.image(d.uri, caption=f'画稿 {idx + 1}')
         counter += 1
-
-    dfav_id_str = st.selectbox(
+    st.selectbox(
         "你满意的精修稿❤️",
-        [''] + list([f'画稿 {i+1}' for i in range(counter)]),
-        index=0)
-    if dfav_id_str:
-        dfav_id = int(dfav_id_str[3:]) - 1
-        dfav = diffused[dfav_id]
+        [''] + list([f'画稿 {i + 1}' for i in range(counter)]),
+        index=0,
+        key='fav_2nd_id_str',
+        on_change=get_from_upscale)
 
 
-if dfav is not None:
-    with st.spinner('正在完成最后的修改🎨...'):
-        final_fav = get_from_upscale(dfav)
-    if final_fav is not None:
+def get_from_upscale():
+    if st.session_state.get('fav_2nd_id_str', ''):
+        dfav_id = int(st.session_state.fav_2nd_id_str[3:]) - 1
+        fav_id = st.session_state.doc.tags['fav_id']
+        dfav = st.session_state.doc.matches[fav_id].matches[dfav_id]
+        st.session_state.doc.tags['dfav_id'] = dfav_id
+        with st.spinner('正在完成最后的修改🎨...'):
+            dfav.post(f'{server_url}/upscale')
+        st.session_state.status = Status.UPSCALE
+    if st.session_state.status.value == Status.UPSCALE.value:
         st.header('大功告成🎉')
-        st.image(final_fav.uri, caption=f'We are driving a red bus to {st.session_state.prompt_raw}')
+        st.image(dfav.uri, caption=f'{st.session_state.doc.tags["description"]}')
+        st.button('retry', on_click=reset_status)
+
+def reset_status():
+    st.session_state.status = Status.PROMPT
+
+
+if 'status' not in st.session_state.keys():
+    st.session_state['status'] = Status.PROMPT
+
+server_url = 'grpcs://dalle-flow.dev.jina.ai'
+
+def plot_tile():
+    st.title('让我们一起给孩子讲故事')
+    # st.header('让我们一起给孩子讲故事')
+    st.subheader('用一句话来完成一个关于开巴士🚌去旅行的故事接龙')
+
+
+def main():
+    get_prompt()
+
+
+main()
